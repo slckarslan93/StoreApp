2c470da3b8e6169c39c519998311cd85bde9ee8b
Selcuk ARSLAN <slckarslan93@gmail.com>
2023-05-30 16:00:51 +0300

    BookRepository

---
+++ Repositories/EFCore/BookRepository.cs
@@ -0,0 +1,30 @@
+﻿using Entities.Models;
+using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore
+{
+    public class BookRepository : RepositoryBase<Book>, IBookRepository
+    {
+        public BookRepository(RepositoryContext context) : base(context)
+        {
+        }
+
+        public void CreateOneBook(Book book) => Create(book);
+
+        public void DeleteOneBook(Book book) => Delete(book);
+
+        public IQueryable<Book> GetAllBooks(bool trackChanges) =>
+            FindAll(trackChanges)
+            .OrderBy(x => x.Id);
+
+        public IQueryable<Book> GetOneBookById(int id, bool trackChanges) =>
+            FindByCondition(x => x.Id.Equals(id), trackChanges);
+
+        public void UpdateOneBook(Book book) => Update(book);
+    }
+}
