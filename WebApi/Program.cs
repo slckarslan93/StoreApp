d66a2405028366b1cacb1ab77e6aa4a28ae4ed80
Selcuk ARSLAN <slckarslan93@gmail.com>
2023-05-30 11:15:16 +0300

    BooksController

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddControllers().AddNewtonsoftJson();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
