73e77e996c790743316eb98af53ba3844c8ef8ab
Selcuk ARSLAN <slckarslan93@gmail.com>
2023-06-08 14:53:19 +0300

    Shaped Entity

---
+++ Entities/LinkModels/LinkCollectionWrapper.cs
@@ -0,0 +1,18 @@
+﻿using static Entities.LinkModels.Link;
+
+namespace Entities.LinkModels
+{
+    public class LinkCollectionWrapper<T> : LinkResourceBase
+    {
+        public List<T> Value { get; set; } = new List<T>();
+
+        public LinkCollectionWrapper(List<T> value)
+        {
+            Value = value;
+        }
+        public LinkCollectionWrapper()
+        {
+
+        }
+    }
+}
