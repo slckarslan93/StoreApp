443328bb40c2d788563b195e88dda25321308caa
Selcuk ARSLAN <slckarslan93@gmail.com>
2023-06-09 17:37:41 +0300

    Custom media Types

--- Entities/LinkModels/LinkResponse.cs
+++ Entities/LinkModels/LinkResponse.cs
@@ -1,10 +1,13 @@
-﻿namespace Entities.LinkModels
+﻿using Entities.Models;
+
+namespace Entities.LinkModels
 {
     public class LinkResponse
     {
         public bool HasLinks { get; set; }
         public List<Entity> ShapedEntities { get; set; }
         public LinkCollectionWrapper<Entity> LinkedEntities { get; set; }
+
         public LinkResponse()
         {
             ShapedEntities = new List<Entity>();
